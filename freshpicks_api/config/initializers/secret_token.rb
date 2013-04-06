0a4db0814b30e674b18e6f8c13daedc448d5e3d2
Ilya Voytov <ilya@voytov.com>
2013-04-06 12:51:44 -0700

    rails api app

---
+++ freshpicks_api/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshpicksApi::Application.config.secret_token = '34e80eca4494633cdcc50c5c3a4ffb61553e4b0aa9f7a528e30d17447cfba33c7d2c97efa97be61a695727e5cde19e875bd389e0ebe937b86d6b357ac7cb1e78'
